03fb218b5f624db1c5f9efab708ab10e4a62b26f
andre <vtavares.andre@gmail.com>
2020-06-16 09:42:04 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CannabisChoice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
